7951ce0adcc3b991118e2856c91ffd447e1338bc
Angela Prestano <angiprestano99@gmail.com>
2023-11-27 16:12:36 +0100

    primo esercizio

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,27 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         // Press Alt+Invio with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Maiusc+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        int oneNumber = 300;
+        int secondNumber = 100;
 
-            // Press Maiusc+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Il prodotto è: " + oneNumber * secondNumber);
+
+        String fraseNuova = "Ciao, oggi è il primo giorno di Backend";
+        int newNumber = 125;
+
+        System.out.println("concatenazione: " + fraseNuova + " " + newNumber);
+
+        String[] arrayDiString = {"A" , "B" , "C" , "D" , "E"};
+        String stringa = "Ciaone";
+
+        String[] secondArray = {arrayDiString[0], arrayDiString[1], stringa,arrayDiString[2],arrayDiString[3], arrayDiString[4]};
+        System.out.println(Arrays.toString(secondArray));
     }
 }
